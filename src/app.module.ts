a4effd040e0fdd98efb37e37dc76580ce6c81e73
calenarrna <calenarrna@gmail.com>
2023-04-12 22:26:35 +0200

    feat(database): basic database connection, entities implemented with TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ReportsModule } from './reports/reports.module';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ReportsModule } from "./reports/reports.module";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/user.entity";
+import { Report } from "./reports/report.entity";
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: "sqlite",
+    database: "db.sqlite",
+    entities: [User, Report],
+    synchronize: true
+  }), UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
