ab068d93f55c31930c09d46315e49dfafdd24389
calenarrna <calenarrna@gmail.com>
2023-04-21 23:42:56 +0200

    feat(exceptions): enhanced exception handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from "@nestjs/common";
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./users.service";
 import { UpdateUserDto } from "./dtos/update-user.dto";
@@ -10,12 +10,16 @@ export class UsersController {
 
   @Post("/signup")
   async createUser(@Body() body: CreateUserDto) {
-    this.userService.create(body.email, body.password);
+    return await this.userService.create(body.email, body.password);
   }
 
   @Get("/:id")
-  findUser(@Param("id") id: string) {
-    return this.userService.findOne(parseInt(id));
+  async findUser(@Param("id") id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException(`User not found with id: ${id}`);
+    }
+    return user;
   }
 
   @Get()
