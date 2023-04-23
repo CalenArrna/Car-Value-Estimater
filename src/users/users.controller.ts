616577e22fe7eeb0726cd599f3efa18367abd0d4
calenarrna <calenarrna@gmail.com>
2023-04-23 22:05:46 +0200

    feat(cookie-session-example): UserController have some examples handling sessions and cookies

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./users.service";
 import { UpdateUserDto } from "./dtos/update-user.dto";
@@ -12,8 +12,18 @@ export class UsersController {
   constructor(private userService: UsersService, private authService: AuthService) {
   }
 
+  @Get("/colors/:color")
+  setColor(@Param("color") color: string, @Session() session: any) {
+    session.color = color;
+  }
+
+  @Get("/colors")
+  getColor(@Session() session: any) {
+    return session.color;
+  }
+
   @Post("/signup")
-   createUser(@Body() body: CreateUserDto) {
+  createUser(@Body() body: CreateUserDto) {
     return this.authService.signup(body.email, body.password);
   }
 
@@ -45,6 +55,5 @@ export class UsersController {
   updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
     return this.userService.update(parseInt(id), body);
   }
-
 
 }
