ec8a8324f68c519fdec2c99b49454a10e1ecd4b0
calenarrna <calenarrna@gmail.com>
2023-04-27 22:48:04 +0200

    feat(reports): create flow implemented

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { ReportsService } from "./reports.service";
+import { CreateReportDto } from "./dtos/create-report.dto";
+import { AuthGuard } from "../guards/auth.guard";
 
-@Controller('reports')
-export class ReportsController {}
+@Controller("reports")
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() report: CreateReportDto) {
+    return this.reportsService.create(report);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { Report } from "./report.entity";
+import { Repository } from "typeorm";
+import { CreateReportDto } from "./dtos/create-report.dto";
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private reportsRepository: Repository<Report>) {
+  }
+  create(dto: CreateReportDto) {
+    const report = this.reportsRepository.create(dto);
+
+    return this.reportsRepository.save(report);
+
+  }
+}
