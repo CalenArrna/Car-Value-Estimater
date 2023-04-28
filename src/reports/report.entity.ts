8440bab7c487564157e6e54a49c00e76575c6cfa
calenarrna <calenarrna@gmail.com>
2023-04-28 11:14:06 +0200

    feat(user,report): OneToMany relation created, database dropped

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { User } from "../users/user.entity";
+
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
@@ -24,4 +26,7 @@ export class Report {
 
   @Column()
   mileage: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
