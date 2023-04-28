72741af306e0f6f27ae3ff8b53a6f33ae4e61624
calenarrna <calenarrna@gmail.com>
2023-04-28 19:37:48 +0200

    feat(approve): reports now can be approved on a patch endpoint

--- src/reports/dtos/report.dto.ts
+++ src/reports/dtos/report.dto.ts
@@ -4,23 +4,33 @@ import { User } from "../../users/user.entity";
 export class ReportDto {
   @Expose()
   id: number;
+
   @Expose()
   price: number;
+
   @Expose()
   year: number;
+
   @Expose()
   lng: number;
+
   @Expose()
   lat: number;
+
   @Expose()
   make: string;
+
   @Expose()
   model: string;
+
   @Expose()
   mileage: number;
 
+  @Expose()
+  approved: boolean;
+
   // obj here stands for the original Report Entity, by destructuring TransformFnParams
-  @Transform(({obj})=> obj.user.id)
+  @Transform(({ obj }) => obj.user.id)
   @Expose()
   userId: number;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, Patch, Param } from "@nestjs/common";
 import { ReportsService } from "./reports.service";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { AuthGuard } from "../guards/auth.guard";
@@ -6,6 +6,7 @@ import { CurrentUser } from "../users/decorators/current-user.decorator";
 import { User } from "../users/user.entity";
 import { Serialize } from "../interceptors/serialize.interceptor";
 import { ReportDto } from "./dtos/report.dto";
+import { ApproveReportDto } from "./dtos/approve-report.dto";
 
 @Controller("reports")
 export class ReportsController {
@@ -17,5 +18,10 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() report: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(report, user);
+  }
+
+  @Patch("/:id")
+  approveReport(@Param("id") id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 }
