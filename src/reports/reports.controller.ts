d8fcb6b23a1991a22330fa814807b9e9b5d3d09a
calenarrna <calenarrna@gmail.com>
2023-04-29 18:18:20 +0200

    fix(get-estimate): fixed issue with query string data fails the validation in dto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, Patch, Param } from "@nestjs/common";
+import { Body, Controller, Post, Get, Patch, Query, UseGuards, Param } from "@nestjs/common";
 import { ReportsService } from "./reports.service";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { AuthGuard } from "../guards/auth.guard";
@@ -8,6 +8,7 @@ import { Serialize } from "../interceptors/serialize.interceptor";
 import { ReportDto } from "./dtos/report.dto";
 import { ApproveReportDto } from "./dtos/approve-report.dto";
 import { AdminGuard } from "../guards/admin.guard";
+import { GetEstimateDto } from "./dtos/get-estimate.dto";
 
 @Controller("reports")
 export class ReportsController {
@@ -25,5 +26,11 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param("id") id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+    return query;
   }
 }
