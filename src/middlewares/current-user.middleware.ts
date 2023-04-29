4c692af0c4e48fa0dd8abdb9999b35ea0ec61eb9
calenarrna <calenarrna@gmail.com>
2023-04-29 17:32:54 +0200

    fix(current-user.middleware): fixed ts-ignore line

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -1,6 +1,16 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import { UsersService } from "../users/users.service";
+import { User } from "../users/user.entity";
+
+declare global{
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
+
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -10,9 +20,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
     if (userId) {
-      const user = await this.userService.findOne(parseInt(userId));
-      // @ts-ignore
-      req.currentUser = user;
+      req.currentUser = await this.userService.findOne(parseInt(userId));
     }
     // REMEMBER TO CALL NEXT()! If this is missing, it will be an infinite loop
     next();
